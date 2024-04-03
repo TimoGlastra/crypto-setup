4272f5e929f9d34b2b0aba6ffb49a7ff163d95e8
Timo Glastra <timo@animo.id>
2024-04-03 15:57:03 +0200

    reproduction
    Signed-off-by: Timo Glastra <timo@animo.id>

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+global.crypto = require("react-native-quick-crypto");
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { v4 } from "uuid";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* This fails once you install @hyperledger/anoncreds-react-native with react-native-quick-crypto */}
+      <Text>UUID: {v4()}</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
